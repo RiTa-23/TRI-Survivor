67b7feead62910379c724af5e8c91dbf18776aa0
hiro2536 <gsksnkk5116@ymobile.ne.jp>
2026-02-11 18:16:09 +0900

    chore: deletedファイルを削除

--- backend/db/migrations/000002_create_settings_table.down.sql
+++
@@ -1,3 +0,0 @@
-DROP TRIGGER IF EXISTS set_settings_updated_at ON settings;
-DROP TABLE IF EXISTS settings;
-DROP FUNCTION IF EXISTS set_updated_at();
--- backend/db/migrations/000003_create_shop_table.down.sql
+++
@@ -1,2 +0,0 @@
-DROP TRIGGER IF EXISTS set_shop_updated_at ON shop;
-DROP TABLE IF EXISTS shop;
